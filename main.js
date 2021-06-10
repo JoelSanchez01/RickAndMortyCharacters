762d1fc860ac0663c8f2b8eba4a9ae4ee892b267
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2021-06-10 16:21:10 -0500

    Se agregaron los elementos por medio de java y se esta utilizando el un pequeno algoritmo para poder lograr utilizar el elemento de busqueda

--- main.js
+++ main.js
@@ -1,55 +1,96 @@
-const url = "https://rickandmortyapi.com/api/character";
+document.addEventListener("DOMContentLoaded", function(event) {
 
-const appNode = document.querySelector(".personajes-Container");
+  const url = "https://rickandmortyapi.com/api/character";
 
-const fetchData = async (API, ID) => {
-  const conectar = await window.fetch(API);
-  const respuesta = await conectar.json();
-  const todos = [];
+  const appNode = document.querySelector(".personajes-Container");
+  const input = document.querySelector('.TituloContainer__form--input');
+  
+  const fetchData = async (API, texto) => {
+    const conectar = await window.fetch(API);
+    const respuesta = await conectar.json();
+    const todos = [];
+    
 
-  try {
-    respuesta.results.forEach((element) => {
-      const imagen = document.createElement("img");
-      imagen.src = element.image;
+      try {
+        respuesta.results.forEach((element) => {
+          
+          const imagen = document.createElement("img");
+          imagen.src = element.image;
 
-      const Name = document.createElement("h4");
-      Name.className = "Name";
-      Name.textContent = element.name;
+          const Name = document.createElement("h4");
+          Name.className = "Name";
+          Name.textContent = element.name;
 
-      const Status = document.createElement("p");
-      Status.className = "Status";
-      Status.textContent = element.status;
+          const Status = document.createElement("p");
+          Status.className = "Status";
+          Status.textContent = element.status;
 
-      const Location = document.createElement("p");
-      Location.className = "Location";
-      Location.textContent = element.location.name;
+          const Location = document.createElement("p");
+          Location.className = "Location";
+          Location.textContent = element.location.name;
 
-      const Gender = document.createElement("p");
-      Gender.className = "Gender";
-      Gender.textContent = element.gender;
+          const Gender = document.createElement("p");
+          Gender.className = "Gender";
+          Gender.textContent = element.gender;
 
-      const Specie = document.createElement("p");
-      Specie.className = "Specie";
-      Specie.textContent = element.species;
+          const Specie = document.createElement("p");
+          Specie.className = "Specie";
+          Specie.textContent = element.species;
 
-      const container = document.createElement("div");
-      container.className = "personajes-Container--card";
+          const container = document.createElement("div");
+          container.className = "personajes-Container--card";
 
+          
+
+          container.append(imagen, Name, Status, Location, Gender, Specie);
+          todos.push(container);
+
+          if (element.id == 20) fetchData(`${url}?page=2`);
+          else if (element.id == 40) fetchData(`${url}?page=3`);
+          else if (element.id == 60) fetchData(`${url}?page=4`);
+          else if (element.id == 80) fetchData(`${url}?page=5`);
+        
+
+        });
+
+        appNode.append(...todos);
+      } catch (error) {
+        console.error(error);
+      }
+    
+  };
+
+  fetchData(url);
+
+  let hola = '';
+
+  const Buscar = (event) => {
+
+    if(event.data == null) {
+      const Tarjetas = document.querySelectorAll('.personajes-Container--card');
+      Tarjetas.forEach((elemento) => {
+        appNode.removeChild(elemento);
+      });
+      hola = input.value;
+      console.log(hola);
+     fetchData(url);
+    }
+    else {
+      const Tarjetas = document.querySelectorAll('.personajes-Container--card');
+      Tarjetas.forEach((elemento) => {
+        appNode.removeChild(elemento);
+      }); 
+      hola+=event.data;
       
-
-      container.append(imagen, Name, Status, Location, Gender, Specie);
-      todos.push(container);
-
-      if (element.id == 20) fetchData(`${url}?page=2`);
-      else if (element.id == 40) fetchData(`${url}?page=3`);
-      else if (element.id == 60) fetchData(`${url}?page=4`);
-      else if (element.id == 80) fetchData(`${url}?page=5`);
-    });
-
-    appNode.append(...todos);
-  } catch (error) {
-    console.error(error);
+      fetchData(`${url}/?name=${hola}`);
+    }
+    
   }
-};
 
-fetchData(url);
+
+  input.addEventListener('input', Buscar);
+
+
+
+
+});
